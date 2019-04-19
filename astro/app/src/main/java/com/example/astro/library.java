99dac29230da05fb4b1efa2d612ba34718c87d31
ajeya-bhat <ajeyabs@gmail.com>
2019-04-19 18:14:28 +0530

    a

--- astro/app/src/main/java/com/example/astro/library.java
+++ astro/app/src/main/java/com/example/astro/library.java
@@ -2,12 +2,19 @@ package com.example.astro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class library extends AppCompatActivity {
+    ListView pdfListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+        pdfListView=(ListView)findViewById(R.id.PDFList);
+        String[] pdfFiles={"Android book","MPCA-1","MPCA-2","MPCA-3","MPCA-4","Database Management Systems","Design and analysis of algorithms",
+        "Linear Algebra"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
     }
 }
