07775b9f557a52c8359bd79e7258dc622f24e4d8
nishith1499 <43772453+nishith1499@users.noreply.github.com>
2019-04-19 21:57:25 +0530

    about us

---
+++ astro/app/src/main/java/com/example/astro/aboutus.java
@@ -0,0 +1,14 @@
+package com.example.astro;
+
+import android.icu.util.RangeValueIterator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aboutus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
+}
